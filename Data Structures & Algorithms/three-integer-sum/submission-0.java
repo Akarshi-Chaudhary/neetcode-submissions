7dcea42d46e642caec6301cfefd12cc61d7c7523
akarshi Chaudhary <akarshichaudhary25@gmail.com>
2026-07-21 23:56:00 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+    //     List<List<Integer>> Ans = new ArrayList<>();
+    //     int n = nums.length;
+
+    //     for(int i = 0; i < n - 2; i++){
+    //         for(int j = i + 1; j < n - 1; j++){
+    //             for(int k = j + 1; k < n; k++){
+    //                 if(nums[i] + nums[j] + nums[k] == 0){
+    //                     Ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return Ans;
+    // }
+
+    Set<List<Integer>>set = new HashSet<>();
+    int n = nums.length;
+    
+    for(int i = 0; i < n - 2; i++){
+        for(int j = i + 1; j < n - 1; j++){
+            for(int k = j + 1; k < n; k++){
+                if(nums[i] + nums[j] + nums[k] == 0){
+                    List<Integer>temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                    Collections.sort(temp);
+                    set.add(temp);
+                }
+            }
+        }
+    }
+    return new ArrayList<>(set);
+    }
+
+}
